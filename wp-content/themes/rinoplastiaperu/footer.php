5e130e4792266a5a40767c8a4c4deeeff1e159e4
jluisj7 <jluisjab7@gmail.com>
2015-03-04 21:43:32 -0500

    footer.php terminado

---
+++ wp-content/themes/rinoplastiaperu/footer.php
@@ -0,0 +1,94 @@
+
+<footer id="main-footer" class="site-footer clearfix">
+    <div class="container">
+        <div class="row">
+
+            <div class=" col-lg-3 col-md-3 col-sm-6  ">
+                <section id="text-2" class="widget animated fadeInLeft widget_text"><h3 class="title">About MedicalPress</h3>			<div class="textwidget"><p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. </p>
+<p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+</div>
+		</section>            </div>
+
+            <div class=" col-lg-3 col-md-3 col-sm-6  ">
+                <section id="displaytweetswidget-2" class="widget animated fadeInLeft widget_displaytweetswidget"><h3 class="title">Latest Tweets</h3><p>Check out 'Cream - Sweets and Bakers WordPress Theme' on <a href="http://www.twitter.com/EnvatoMarket" target="_blank">@EnvatoMarket</a> by <a href="http://www.twitter.com/InspiryThemes" target="_blank">@InspiryThemes</a> <a href="http://twitter.com/search?q=%23themeforest&src=hash" target="_blank">#themeforest</a> <a href="http://t.co/we8M81zKf0" target="_blank">http://t.co/we8M81zKf0</a><br /><small class="muted">- Thursday Nov 6 - 7:25am</small></p><p>Data Sanitization and Validation With WordPress <a href="http://t.co/AJJoOERrwp" target="_blank">http://t.co/AJJoOERrwp</a> via <a href="http://www.twitter.com/tutsplus" target="_blank">@tutsplus</a><br /><small class="muted">- Sunday Nov 2 - 8:02am</small></p><p>Testing an amazing WooCommerce theme that I am just about to launch in few days. It is oriented toward food industry. Stay tuned...<br /><small class="muted">- Thursday Oct 23 - 5:05am</small></p></section>            </div>
+
+            <div class="clearfix visible-sm"></div>
+
+            <div class=" col-lg-3 col-md-3 col-sm-6  ">
+                		<section id="recent-posts-3" class="widget animated fadeInLeft widget_recent_entries">		<h3 class="title">Recent Posts</h3>		<ul>
+					<li>
+				<a href="http://inspirythemesdemo.com/medicalpress/image-post-format/">Image Post Format</a>
+						</li>
+					<li>
+				<a href="http://inspirythemesdemo.com/medicalpress/quote-post-format/">Quote Post Format</a>
+						</li>
+					<li>
+				<a href="http://inspirythemesdemo.com/medicalpress/gallery-post-format/">Gallery Post Format</a>
+						</li>
+					<li>
+				<a href="http://inspirythemesdemo.com/medicalpress/how-to-live-a-healthier-life/">How to Live a Healthier Life</a>
+						</li>
+					<li>
+				<a href="http://inspirythemesdemo.com/medicalpress/the-hero-in-all-of-us/">The Hero In All Of Us</a>
+						</li>
+				</ul>
+		</section>            </div>
+
+            <div class=" col-lg-3 col-md-3 col-sm-6  ">
+                <section id="wp_email_capture_widget_class-2" class="widget animated fadeInLeft widget_wp_email_capture_widget_class"><h3 class="title">Newsletter Sign Up </h3><div class="textwidget">Stay updated with latest news from Medical Press.</div> <div id="wp_email_capture" class="wp-email-capture wp-email-capture-widget wp-email-capture-widget-worldwide"><form name="wp_email_capture" method="post" action="http://inspirythemesdemo.com/medicalpress/">
+
+ 	
+	<label class="wp-email-capture-name wp-email-capture-label wp-email-capture-widget-worldwide wp-email-capture-name-widget wp-email-capture-name-label wp-email-capture-name-label-widget" for="wp-email-capture-name-widget">Name:</label> <input name="wp-email-capture-name" id="wp-email-capture-name-widget" type="text" class="wp-email-capture-name wp-email-capture-input wp-email-capture-widget-worldwide wp-email-capture-name-widget wp-email-capture-name-input wp-email-capture-name-input-widget" title="Name" /><br/>
+
+	<label class="wp-email-capture-email wp-email-capture-label wp-email-capture-widget-worldwide wp-email-capture-email-widget wp-email-capture-email-label wp-email-capture-email-label-widget"  for="wp-email-capture-email-widget">Email:</label> <input name="wp-email-capture-email" id="wp-email-capture-email-widget" type="text" class="wp-email-capture-email wp-email-capture-input wp-email-capture-widget-worldwide wp-email-capture-email-widget wp-email-capture-email-input wp-email-capture-email-input-widget" title="Email" /><br/>
+
+	<input type="hidden" name="wp_capture_action" value="1" />
+
+<input name="Submit" type="submit" value="Submit" class="wp-email-capture-submit wp-email-capture-widget-worldwide" />
+
+</form>
+
+</div>
+
+</section>            </div>
+
+            <div class="col-lg-12 col-md-12 col-sm-12 ">
+                <div class="footer-bottom animated fadeInDown clearfix">
+                    <div class="row">
+                                                    <div class="col-lg-7 col-md-7 col-sm-7 col-xs-12 ">
+                                <p>© Copyright 2014. All Rights Reserved by Medical Press</p>
+                            </div>
+                                                    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-12  clearfix">
+                                <ul class="footer-social-nav">
+                                    <li><a target="_blank" href="https://twitter.com/InspiryThemes"><i class="fa fa-twitter"></i></a></li><li><a target="_blank" href="https://www.facebook.com/InspiryThemes"><i class="fa fa-facebook"></i></a></li><li><a target="_blank" href="https://plus.google.com/105667798414314247471"><i class="fa fa-google-plus"></i></a></li><li><a target="_blank" href="#linkedin"><i class="fa fa-linkedin"></i></a></li><li><a target="_blank" href="#instagram"><i class="fa fa-instagram"></i></a></li><li><a target="_blank" href="#youtube"><i class="fa fa-youtube"></i></a></li><li><a target="_blank" href="skype:skypeusername?add"><i class="fa fa-skype"></i></a></li><li><a target="_blank" href="http://inspirythemesdemo.com/medicalpress/feed/"><i class="fa fa-rss"></i></a></li>                                </ul>
+                            </div>
+                                            </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<a href="#top" id="scroll-top"></a>
+
+<script type='text/javascript' id='quick-js'>
+                                    
+</script>
+
+<script type='text/javascript' src='<?php bloginfo('template_url'); ?>/js/bootstrap.min.js?ver=3.1.0'></script>
+<script type='text/javascript' src='<?php bloginfo('template_url'); ?>/js/jquery.flexslider-min.js?ver=2.3.0'></script>
+<script type='text/javascript' src='<?php bloginfo('template_url'); ?>/js/jquery.swipebox.min.js?ver=1.2.1'></script>
+<script type='text/javascript' src='<?php bloginfo('template_url'); ?>/js/jquery.isotope.min.js?ver=1.5.25'></script>
+<script type='text/javascript' src='<?php bloginfo('template_url'); ?>/js/jquery.appear.js?ver=0.3.3'></script>
+<script type='text/javascript' src='<?php bloginfo('template_url'); ?>/js/core.min.js?ver=1.11.2'></script>
+<script type='text/javascript' src='<?php bloginfo('template_url'); ?>/js/datepicker.min.js?ver=1.11.2'></script>
+<script type='text/javascript' src='<?php bloginfo('template_url'); ?>/js/jquery.validate.min.js?ver=1.11.1'></script>
+<script type='text/javascript' src='<?php bloginfo('template_url'); ?>/js/jquery.form.min.js?ver=3.37.0'></script>
+<script type='text/javascript' src='<?php bloginfo('template_url'); ?>/js/jquery.jplayer.min.js?ver=2.6.0'></script>
+<script type='text/javascript' src='<?php bloginfo('template_url'); ?>/js/jquery.autosize.min.js?ver=1.18.7'></script>
+<script type='text/javascript' src='<?php bloginfo('template_url'); ?>/js/jquery.meanmenu.min.js?ver=2.0.6'></script>
+<script type='text/javascript' src='<?php bloginfo('template_url'); ?>/js/jquery.velocity.min.js?ver=0.0.0'></script>
+<script type='text/javascript' src='<?php bloginfo('template_url'); ?>/js/comment-reply.min.js?ver=4.1.1'></script>
+<script type='text/javascript' src='<?php bloginfo('template_url'); ?>/js/custom.js?ver=1.0'></script>
+</body>
+<?php wp_footer(); ?>
+</html>
